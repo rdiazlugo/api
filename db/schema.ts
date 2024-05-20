29f336bfcdd1b8ed77bc0db54cda23c09758e992
Reinaldo Diaz Lugo <reinaldo122@gmail.com>
2024-05-20 19:19:46 +0200

    update schema with pull from DB

--- db/schema.ts
+++ db/schema.ts
@@ -1,17 +1,34 @@
-import { pgTable, timestamp, text, json, unique } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  timestamp,
+  text,
+  uniqueIndex,
+  jsonb,
+} from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { generatePublicId } from "./utils";
 
 const timestapms = {
-  createdAt: timestamp("createdAt").defaultNow(),
-  updatedAt: timestamp("updatedAt").$onUpdate(() => new Date()),
+  createdAt: timestamp("createdAt", {
+    precision: 3,
+    mode: "string",
+  }).defaultNow(),
+  updatedAt: timestamp("updatedAt", { precision: 3, mode: "string" }).$onUpdate(
+    () => new Date().toISOString()
+  ),
 };
 
-export const User = pgTable("User", {
-  id: text("id").primaryKey().$defaultFn(generatePublicId),
-  email: text("email").notNull().unique(),
-  ...timestapms,
-});
+export const User = pgTable(
+  "User",
+  {
+    id: text("id").primaryKey().$defaultFn(generatePublicId),
+    email: text("email").notNull().unique(),
+    ...timestapms,
+  },
+  (t) => ({
+    emailKey: uniqueIndex("User_email_key").on(t.email),
+  })
+);
 
 export const userRelations = relations(User, ({ many }) => ({
   tokens: many(ApiToken),
@@ -19,14 +36,22 @@ export const userRelations = relations(User, ({ many }) => ({
   devices: many(UserDevice),
 }));
 
-export const ApiToken = pgTable("ApiToken", {
-  id: text("id").primaryKey().$defaultFn(generatePublicId),
-  description: text("description").notNull(),
-  key: text("key"),
-  private_key: text("private_key").notNull().unique(),
-  userId: text("userId").notNull(),
-  ...timestapms,
-});
+export const ApiToken = pgTable(
+  "ApiToken",
+  {
+    id: text("id").primaryKey().$defaultFn(generatePublicId),
+    description: text("description").notNull(),
+    key: text("key"),
+    privateKey: text("private_key").notNull().unique(),
+    userId: text("userId")
+      .notNull()
+      .references(() => User.id, { onDelete: "cascade", onUpdate: "cascade" }),
+    ...timestapms,
+  },
+  (t) => ({
+    privateKeyKey: uniqueIndex("ApiToken_private_key_key").on(t.privateKey),
+  })
+);
 
 export const apiTokenRelations = relations(ApiToken, ({ one }) => ({
   user: one(User, {
@@ -41,42 +66,35 @@ export const SocialLogin = pgTable(
     id: text("id").primaryKey().$defaultFn(generatePublicId),
     provider: text("provider").notNull(),
     email: text("email").notNull(),
-    userId: text("userId").notNull(),
-    profile: json("profile"),
-    tokens: json("tokens"),
+    userId: text("userId")
+      .notNull()
+      .references(() => User.id, { onDelete: "cascade", onUpdate: "cascade" }),
+    profile: jsonb("profile"),
+    tokens: jsonb("tokens"),
     ...timestapms,
   },
   (t) => ({
-    unq: unique("SocialLogin_email_provider_key").on(t.email, t.provider),
+    emailProviderKey: uniqueIndex("SocialLogin_email_provider_key").on(
+      t.provider,
+      t.email
+    ),
   })
 );
 
-export const socialLoginRelations = relations(SocialLogin, ({ one }) => ({
-  user: one(User, {
-    fields: [SocialLogin.userId],
-    references: [User.id],
-  }),
-}));
-
-export const AuditLog = pgTable("audit_logs", {
-  id: text("id").primaryKey().$defaultFn(generatePublicId),
-  timestamp: timestamp("timestamp").defaultNow(),
-  keyId: text("key_id").notNull(),
-  auditType: text("audit_type", { enum: ["api", "webhook"] }).notNull(),
-  auditAction: text("audit_action", {
-    enum: ["read", "write", "delete"],
-  }).notNull(),
-  data: json("data"),
-});
-
-export const InvalidLogin = pgTable("InvalidLogin", {
-  id: text("id").primaryKey().$defaultFn(generatePublicId),
-  provider: text("provider").notNull(),
-  email: text("email").notNull(),
-  profile: json("profile"),
-  tokens: json("tokens"),
-  ...timestapms,
-});
+export const InvalidLogin = pgTable(
+  "InvalidLogin",
+  {
+    id: text("id").primaryKey().$defaultFn(generatePublicId),
+    provider: text("provider").notNull(),
+    email: text("email").notNull(),
+    profile: jsonb("profile"),
+    tokens: jsonb("tokens"),
+    ...timestapms,
+  },
+  (t) => ({
+    emailKey: uniqueIndex("InvalidLogin_email_key").on(t.email),
+  })
+);
 
 export const UserDevice = pgTable(
   "UserDevice",
@@ -84,14 +102,16 @@ export const UserDevice = pgTable(
     id: text("id").primaryKey().$defaultFn(generatePublicId),
     userId: text("userId")
       .notNull()
-      .references(() => User.id),
+      .references(() => User.id, { onDelete: "cascade", onUpdate: "cascade" }),
     name: text("name").notNull(),
     userAgent: text("userAgent").notNull(),
-
     ...timestapms,
   },
   (t) => ({
-    unq: unique("UserDevice_userId_userAgent_key").on(t.userId, t.userAgent),
+    userIdUserAgentKey: uniqueIndex("UserDevice_userId_userAgent_key").on(
+      t.userAgent,
+      t.userId
+    ),
   })
 );
 
@@ -109,18 +129,20 @@ export const PushSubscription = pgTable(
     id: text("id").primaryKey().$defaultFn(generatePublicId),
     deviceId: text("deviceId")
       .notNull()
-      .references(() => UserDevice.id),
+      .references(() => UserDevice.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
     endpoint: text("endpoint").notNull(),
     p256dh: text("p256dh").notNull(),
     auth: text("auth").notNull(),
     ...timestapms,
   },
   (t) => ({
-    unq: unique("PushSubscription_deviceId_endpoint_auth_key").on(
-      UserDevice.id,
-      t.endpoint,
-      t.auth
-    ),
+    deviceIdKey: uniqueIndex("PushSubscription_deviceId_key").on(t.deviceId),
+    deviceIdEndpointAuthKey: uniqueIndex(
+      "PushSubscription_deviceId_endpoint_auth_key"
+    ).on(t.endpoint, t.deviceId, t.auth),
   })
 );
 
