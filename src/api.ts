e28c9fac3229949aebd85561be0ff9dd07f89b31
Reinaldo Diaz Lugo <reinaldo122@gmail.com>
2024-05-25 14:25:30 +0200

    uses schema from @rdiazlugo/db-schema package

--- src/api.ts
+++ src/api.ts
@@ -6,4 +6,11 @@ api.get("/", (c) => {
   return c.json({ message: "Hello, World!" });
 });
 
+api.post("/file", async (c) => {
+  console.log(c.req.header());
+  const body = await c.req.parseBody();
+  console.log(body["file"]);
+  return c.json({ message: "File received" });
+});
+
 export default api;
